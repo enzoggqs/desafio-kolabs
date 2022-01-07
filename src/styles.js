e0ff2d0460a33d5e00b212beb94d79325f5e26e2
Enzo Gabriel <eggqsaraiva@gmail.com>
2022-01-07 00:11:02 -0300

    Excluding app.css file and using styled components instead

---
+++ src/styles.js
@@ -0,0 +1,17 @@
+import styled from "styled-components";
+
+export const MainDiv = styled.div`
+    text-align: center;
+    justify-content: center;
+    height: 100%;
+    margin-top: 5%;
+    padding: auto;
+
+    span {
+        font-size: 72px;
+    }
+
+    font-size: 144px;
+    
+    font-weight: bold;
+`
