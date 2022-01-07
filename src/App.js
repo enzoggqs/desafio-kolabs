5b1bbecb3a2bc8031b8988558d9518396ce906a9
Enzo Gabriel <eggqsaraiva@gmail.com>
2022-01-07 00:13:25 -0300

    Making first draft of the background

--- src/App.js
+++ src/App.js
@@ -1,10 +1,97 @@
-import './App.css';
+import Particles from "react-tsparticles";
+import { Login } from "./pages/Login";
+import { MainDiv } from './styles';
 
-function App() {
+export const App = () => {
+  const particlesInit = (main) => {
+    console.log(main);
+  };
+
+  const particlesLoaded = (container) => {
+    console.log(container);
+  };
   return (
-    <div className="App">
-    </div>
+    <MainDiv>
+        <span>CREATE</span>
+        <br/>
+        TECHNOLOGY
+      <Particles
+        id="tsparticles"
+        init={particlesInit}
+        loaded={particlesLoaded}
+        options={{
+          fpsLimit: 30,
+          interactivity: {
+            events: {
+              onClick: {
+                enable: true,
+                mode: "push",
+              },
+              onHover: {
+                enable: true,
+                mode: "grab",
+              },
+              resize: true,
+            },
+            modes: {
+              bubble: {
+                distance: 100,
+                duration: 15,
+                opacity: 1,
+                size: 40,
+              },
+              push: {
+                quantity: 4,
+              },
+              repulse: {
+                distance: 200,
+                duration: 0.4,
+              },
+            },
+          },
+          particles: {
+            color: {
+              value: "#111111",
+            },
+            links: {
+              color: "#111111",
+              distance: 150,
+              enable: true,
+              opacity: 0.5,
+              width: 1,
+            },
+            collisions: {
+              enable: true,
+            },
+            move: {
+              direction: "none",
+              enable: true,
+              outMode: "bounce",
+              random: true,
+              speed: 3,
+              straight: false,
+            },
+            number: {
+              density: {
+                enable: true,
+                area: 800,
+              },
+              value: 80,
+            },
+            opacity: {
+              value: 0.5,
+            },
+            shape: {
+              type: "circle",
+            },
+            size: {
+              random: true,
+              value: 5,
+            },
+          },
+          detectRetina: true,
+        }}
+      />
+    </MainDiv>
   );
 }
-
-export default App;
