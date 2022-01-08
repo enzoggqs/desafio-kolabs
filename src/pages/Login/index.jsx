200936d3a08d7e7752d21af5591ef6341cd94ecc
Enzo Gabriel <eggqsaraiva@gmail.com>
2022-01-08 01:26:00 -0300

    Updating login component

--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -1,13 +1,29 @@
 import { LoginContainer } from "../../styles"
+import logo from '../../assets/logo-removebg-preview (2).png';
+import { useState } from "react";
 
 export const Login = () => {
+    const [userFounded] = useState(false);
+    const [passwordMatches] = useState(false);
+
     return (
         <LoginContainer>
-            <img src="../../assets/logo.jpeg" alt="" />
+            <img src={logo} alt="logo" />
             <h1>LOGIN</h1>
             <form>
-
+                <label>E-MAIL / USERNAME</label>
+                <input type="email"></input>
+                {!userFounded &&
+                    <span>USER NÃO ENCONTRADO</span>
+                }
+                <label>SENHA</label>
+                <input type="password"></input>
+                {!passwordMatches &&
+                    <span>SENHA NÃO CONFERE</span>
+                }
+                <button type="submit">ENTRAR</button>
             </form>
+            <a href="#">DON'T HAVE AN ACCOUNT?</a>
         </LoginContainer>
     )
 }
